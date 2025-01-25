0bd4d02baab81074eeb390f5a994d9a406f9160e
Kiran Dowluru <kirandk@outlook.com>
2025-01-25 12:33:25 -0800

    browse the mounted filepath

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
 app = Flask(__name__)
-
+vecstore_path =  '/home/filesharemount'
 
 @app.route('/')
 def index():
@@ -18,10 +18,15 @@ def favicon():
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   
    name = request.args.get('name')
    city = request.args.get('city')
-   return f'Hello, {name} from {city}, ! Welcome to the world of Flask!'
+   filenames = []
+   for root, dirs, files in os.walk(vecstore_path):
+       for file in files:
+           print(file)
+           filenames.append(file)
+
+   return f'Hello, {name} from {city}, ! Welcome to the world of Flask! We have {len(filenames)} files in the filesharemount directory.'
 
    
 if __name__ == '__main__':
