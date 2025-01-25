f06a556c8c5953f8bc1717d0be1b0662493d9c31
Kiran Dowluru <kirandk@outlook.com>
2025-01-25 12:57:14 -0800

    removed static files, added a new route for /chat

--- app.py
+++ app.py
@@ -1,23 +1,13 @@
 import os
 
-from flask import (Flask, redirect, render_template, request,
+from flask import (Flask, redirect, render_template, request,jsonify,
                    send_from_directory, url_for)
 
 app = Flask(__name__)
 vecstore_path =  '/home/filesharemount'
 
 @app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
+def mainPage():
    name = request.args.get('name')
    city = request.args.get('city')
    filenames = []
@@ -26,8 +16,21 @@ def hello():
            print(file)
            filenames.append(file)
 
-   return f'Hello, {name} from {city}, ! Welcome to the world of Flask! We have {len(filenames)} files in the filesharemount directory.'
+   return f'Hello there, ! Welcome to the Medcopilot, the medical guidelines assistant! '
 
-   
+
+@app.route('/chat', methods=['POST'])
+def chat():
+   message = request.args.get('message')
+   print(f'Request for medcopilot /chat received message={message}')
+   '''
+   inference = Inference(storeLocation=vecstore_path)
+   inference.create_rag_chains()
+   response = inference.run_inference(message)
+   print('Response from inference:', response)
+   response = ResultParser(response).serialize()
+   '''
+   return jsonify("Canned response for message: " + message) 
+
 if __name__ == '__main__':
    app.run()
