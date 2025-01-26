e684442076f81a9b6abd5e2d78fedb3253ee14e3
Kiran Dowluru <kirandk@outlook.com>
2025-01-26 12:06:48 -0800

    added sqllite3

--- reference/runinference.py
+++ reference/runinference.py
@@ -3,6 +3,9 @@
 #from tkinter import scrolledtext
 from datetime import datetime
 #import openai
+import pysqlite3
+import sys
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
