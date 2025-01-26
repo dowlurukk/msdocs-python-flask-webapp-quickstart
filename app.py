e2512d8991f039d94dd82332ffabbf3405f504e5
Kiran Dowluru <kirandk@outlook.com>
2025-01-26 10:46:37 -0800

    update the import

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from runinference import Inference
+import Inference
 from flask import (Flask, redirect, render_template, request,jsonify,
                    send_from_directory, url_for)
 
