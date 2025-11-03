3a21354821004398eb239a8b2ce876eb7f7dd072
Kiran dowluru <kirandk@outlook.com>
2025-11-02 19:12:40 -0800

    updating for the prompt change

--- reference/runinference2.py
+++ reference/runinference2.py
@@ -15,7 +15,7 @@ if sys.platform == "linux":
     except ImportError:
         pass  # Fall back to built-in sqlite3
 
-from promptcategories import PromptCategories
+from .promptcategories import PromptCategories
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
